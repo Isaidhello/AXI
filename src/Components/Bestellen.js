ed52417ef92dc1356925615684aa135ed914ec0e
Furkan Öztürk <furkan___01@live.nl>
2020-11-23 11:52:34 +0100

    Bestellen component aangemaakt

---
+++ src/Components/Bestellen.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    topBar: {
+        height: "7vh"
+    }
+}));
+
+function Bestellen() {
+    const classes = useStyles();
+    return (
+        <Grid container>
+            {/* Top bar */}
+            <Grid container item xs={12} className={classes.topBar}>
+
+            </Grid>
+            {/* Content */}
+            <Grid container>
+                {/* Searchbar */}
+                <Grid item container>
+
+                </Grid>
+                {/* List met gescande producten */}
+                <Grid item>
+
+                </Grid>
+                {/* Scan een (volgend) product balk */}
+                <Grid item container>
+
+                </Grid>
+            </Grid>
+        </Grid>
+    )
+}
+
+export default Bestellen;
