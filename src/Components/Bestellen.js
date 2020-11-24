aa09d9a7563367cc8b99e6082ff88da97b6864f0
Furkan Öztürk <furkan___01@live.nl>
2020-11-24 10:19:05 +0100

    Update bestellen.js

--- src/Components/Bestellen.js
+++ src/Components/Bestellen.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Card, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Headerflow from './HeaderFlow';
+import SearchBar from "material-ui-search-bar";
+import ProductList from './components/afprijzen/swipeable/BasicExample';
 
 
 const useStyles = makeStyles((theme) => ({
     topBar: {
         height: "7vh"
+    },
+    scan: {
+        backgroundColor: "#FE9D02",
+        color: "white",
+        width: "80vw",
+        marginTop: "20px"
+    },
+    scanText: {
+        padding: "10px"
+    },
+    searchbar: {
+        marginTop: "10px",
+    },
+    productList: {
+
     }
 }));
 
@@ -16,21 +33,32 @@ function Bestellen() {
         <Grid container>
             {/* Top bar */}
             <Grid container item xs={12} className={classes.topBar}>
-            <Headerflow />
+                <Headerflow />
             </Grid>
             {/* Content */}
             <Grid container>
                 {/* Searchbar */}
-                <Grid item container>
-
+                <Grid item container justify="center" className={classes.searchbar}>
+                    <SearchBar
+                        placeholder='Scan of zoek hier een product'
+                        style={{
+                            width: 650,
+                            height: 60,
+                            backgroundcolor: "orange",
+                        }}
+                    />
                 </Grid>
                 {/* List met gescande producten */}
-                <Grid item>
-
+                <Grid item xs={12} className={classes.productList}>
+                    <ProductList></ProductList>
                 </Grid>
                 {/* Scan een (volgend) product balk */}
-                <Grid item container>
-
+                <Grid item container xs={12} justify="center" alignItems="center">
+                    <Card raised={false} className={classes.scan} align="center">
+                        <Typography variant="h6" className={classes.scanText}>
+                            Scan een product
+                        </Typography>
+                    </Card>
                 </Grid>
             </Grid>
         </Grid>
