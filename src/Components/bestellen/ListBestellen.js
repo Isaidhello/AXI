7d12ecad1a1aa6ebbf3b4fe83602ddb48b839894
Furkan Öztürk <furkan___01@live.nl>
2020-11-26 15:41:01 +0100

    List fix + FAB dialog box

--- src/Components/bestellen/ListBestellen.js
+++ src/Components/bestellen/ListBestellen.js
@@ -10,17 +10,17 @@ import { Typography } from '@material-ui/core';
 import TableCell from '@material-ui/core/TableCell';
 
 import Divider from '@material-ui/core/Divider';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
     divider: {
         // Theme Color, or use css color in quote
         background: 'grey',
-        height: '3px'
+        height: '1px'
     },
-  }));
+}));
 
-  export default function ListBestellen() {
+export default function ListBestellen() {
 
     const classes = useStyles();
 
@@ -28,127 +28,174 @@ const useStyles = makeStyles((theme) => ({
         <div>
             <List>
                 <ListItem className="listitem">
-                    <Link to="/bestellen1" style={{textDecoration:'none', color: 'black'}}>
-                    <ListItemAvatar className="listItemAvatar">
-                        
-                    </ListItemAvatar>
-                    <ListItemText className="listItemText">
-                    <Typography variant="h5">
-                            <TableCell className="cell1">&nbsp;</TableCell>
-                            <TableCell className="cell2"></TableCell>
-                    </Typography>
-                    </ListItemText>
-                    <ListItemSecondaryAction className="editicon">
-                    </ListItemSecondaryAction>
+                    <Link to="/bestellen1" style={{ textDecoration: 'none', color: 'black' }}>
+                        <ListItemAvatar className="listItemAvatar">
+
+                        </ListItemAvatar>
+                        <ListItemText className="listItemText">
+                            <Typography variant="h5">
+                                <TableCell className="cell1">&nbsp;</TableCell>
+                                <TableCell className="cell2"></TableCell>
+                            </Typography>
+                        </ListItemText>
+                        <ListItemSecondaryAction className="editicon">
+                        </ListItemSecondaryAction>
                     </Link>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
+                <Divider classes={{ root: classes.divider }} />
 
                 <ListItem className="listitem">
                     <ListItemAvatar className="listItemAvatar">
-                        
+
                     </ListItemAvatar>
                     <ListItemText className="listItemText">
-                    <Typography variant="h5">
+                        <Typography variant="h5">
                             <TableCell className="cell1">&nbsp;</TableCell>
                             <TableCell className="cell2"></TableCell>
-                    </Typography>
+                        </Typography>
                     </ListItemText>
                     <ListItemSecondaryAction className="editicon">
                     </ListItemSecondaryAction>
                 </ListItem>
-                <Divider classes={{root: classes.divider}} />
-                
+                <Divider classes={{ root: classes.divider }} />
+
+                <ListItem className="listitem">
+                    <ListItemAvatar className="listItemAvatar">
+
+                    </ListItemAvatar>
+                    <ListItemText className="listItemText">
+                        <Typography variant="h5">
+                            <TableCell className="cell1">&nbsp;</TableCell>
+                            <TableCell className="cell2"></TableCell>
+                        </Typography>
+                    </ListItemText>
+                    <ListItemSecondaryAction className="editicon">
+                    </ListItemSecondaryAction>
+                </ListItem>
+                <Divider classes={{ root: classes.divider }} />
+
+                <ListItem className="listitem">
+                    <ListItemAvatar className="listItemAvatar">
+
+                    </ListItemAvatar>
+                    <ListItemText className="listItemText">
+                        <Typography variant="h5">
+                            <TableCell className="cell1">&nbsp;</TableCell>
+                            <TableCell className="cell2"></TableCell>
+                        </Typography>
+                    </ListItemText>
+                    <ListItemSecondaryAction className="editicon">
+                    </ListItemSecondaryAction>
+                </ListItem>
+                <Divider classes={{ root: classes.divider }} />
+
+                <ListItem className="listitem">
+                    <ListItemAvatar className="listItemAvatar">
+
+                    </ListItemAvatar>
+                    <ListItemText className="listItemText">
+                        <Typography variant="h5">
+                            <TableCell className="cell1">&nbsp;</TableCell>
+                            <TableCell className="cell2"></TableCell>
+                        </Typography>
+                    </ListItemText>
+                    <ListItemSecondaryAction className="editicon">
+                    </ListItemSecondaryAction>
+                </ListItem>
+                <Divider classes={{ root: classes.divider }} />
+
+
+
             </List>
         </div>
     )
--- src/components/afprijzen/FABRechtsOnder.js
+++ src/components/afprijzen/FABRechtsOnder.js
@@ -4,33 +4,73 @@ import Fab from '@material-ui/core/Fab';
 //import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 import DoneIcon from '@material-ui/icons/Done';
 import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 const useStyles = makeStyles((theme) => ({
-    root:{
-        display: 'grid',
-    },
-    item: {
-        position: 'absolute',
-        bottom: '40px',
-        right: '40px',
-        borderRadius: '0%',
-        width: '70px',
-        height: '70px',
-    },
-    arrow: {
-      color: 'white',
-      fontSize: 35
-    }
+  root: {
+    display: 'grid',
+  },
+  item: {
+    position: 'absolute',
+    bottom: '40px',
+    right: '40px',
+    borderRadius: '0%',
+    width: '70px',
+    height: '70px',
+  },
+  arrow: {
+    color: 'white',
+    fontSize: 35
+  }
+
 }));
 
 export default function FABRechtsOnder() {
-    const classes = useStyles();
-  
-    return (
-      <div className={classes.root} ><Link to='/home'>
-        <Fab className={classes.item} aria-label="add" color="secondary">
-          <DoneIcon className={classes.arrow} fontSize='inherit' />
-        </Fab></Link>
-      </div>
-    );
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  }
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} >
+      <Fab className={classes.item} aria-label="add" color="secondary" onClick={handleClickOpen}>
+        <DoneIcon className={classes.arrow} fontSize='inherit' />
+
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Weet je zeker dat je lijst wilt opsturen?"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Als je op bevestigen drukt wordt de lijst opgestuurd. Dit kan niet ongedaan worden.
+          </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose} color="primary" >
+              Annuleren
+          </Button>
+            <Link to='/home'>
+              <Button onClick={handleClose} color="primary" autoFocus>
+                Bevestigen
+              </Button>
+            </Link>
+          </DialogActions>
+        </Dialog>
+      </Fab>
+    </div>
+  );
 }
